becfd5725356dc276c6134f93afd977e0ee0a147
LeonidLodygin <st108157@student.spbu.ru>
2022-10-22 19:00:33 +0300

    Added new type and 2 functions

---
+++ src/Homeworks/Tree.fs
@@ -0,0 +1,36 @@
+﻿module TreeHomework
+open System.Collections.Generic
+open MyListHomework
+
+
+type ArbitraryTree<'value> =
+    | Node of value : 'value * nodes : array<ArbitraryTree<'value>>
+    | Leaf of value : 'value
+
+let TrainingTree: ArbitraryTree<int> = Node(12, [|Node(1, [|Leaf(12)|]); Node(3, [|Leaf(12)|]); Node(10, [|Node(4, [|Leaf(12)|])|]); Node(15, [|Leaf(12);Leaf(1)|])|])
+
+let NumOfDifferent (tree:ArbitraryTree<'value>) =
+    let hashSet = new HashSet<'value>()
+    let rec Counter tree =
+        match tree with
+        | Leaf value ->
+            hashSet.Add(value) |> ignore
+        | Node (value, array) ->
+            for i in array do
+               Counter i
+            hashSet.Add(value) |> ignore
+    Counter(tree)
+    hashSet.Count
+
+let rec MyListOfTree (tree:ArbitraryTree<'value>) =
+    let rec Filler tree =
+        match tree with
+        | Leaf value ->
+            Cons(value, Empty)
+        | Node (value, array) ->
+            let rec ArrayOfTreesToMyList (array: array<ArbitraryTree<'value>>) =
+                match array with
+                | [||] -> Empty
+                | array -> Concatenation (MyListOfTree array[0]) (ArrayOfTreesToMyList array[1 .. array.Length - 1])
+            Cons(value, ArrayOfTreesToMyList array)
+    Filler tree
