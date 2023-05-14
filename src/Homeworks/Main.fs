474c09298f05bb83ae31326113224665cdd15492
LeonidLodygin <st108157@student.spbu.ru>
2023-05-14 17:23:37 +0300

    Matrix reader

--- src/Homeworks/Main.fs
+++ src/Homeworks/Main.fs
@@ -1,6 +1,14 @@
 namespace Homeworks
 
+open GraphBuild
+
 module Main =
 
     [<EntryPoint>]
-    let main (argv: string array) = 0
+    let main (argv: string array) =
+        let x = System.Convert.ChangeType("1,1", typeof<float>)
+        printfn $"%A{x}"
+        let matrix = MatrixReader "C:\Users\Леонид\Desktop\hello2.mtx"
+        for i in 1 .. 100 do
+            printfn("Hello, world!")
+        0
